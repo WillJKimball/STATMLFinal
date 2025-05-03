6fb30ebe642ee07ac2f6a42a362998e0a81b1ff1
Will Kimball <willkimball8@gmail.com>
2025-05-03 18:29:10 -0400

    Updated header

--- basketball_predictor.py
+++ basketball_predictor.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from xgboost import XGBRegressor
 from kenpompy.utils import login
-from kenpompy import summary, misc
+from kenpompy import summary
 
 
 class BasketballPredictor:
